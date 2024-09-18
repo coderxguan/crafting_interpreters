71071b7c717eb5bb460d62b217617305734c9a91
coderxguan <3288733739@qq.com>
2024-09-18 19:14:39 +0800

    complete panic mode 7.1

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -17,6 +17,14 @@ class Parser {
         this.tokens = tokens;
     }
 
+    Expr parse() {
+        try {
+            return expression();
+        }catch (ParseError e) {
+            return null;
+        }
+    }
+
     private Expr expression() {
         return equality();
     }
@@ -86,7 +94,7 @@ class Parser {
             consume(RIGHT_PAREN, "Expect ')' after expression.");
             return new Expr.Grouping(expr);
         }
-        return null;
+        throw error(peek(), "Expect expression.");
     }
 
     private boolean match(TokenType... types) {
@@ -126,5 +134,25 @@ class Parser {
         Lox.error(token, message);
         return new ParseError();
     }
+    private void synchronize() {
+        advance();
+         while(!isAtEnd()) {
+             if(previous().type == SEMICOLON)
+                 return;
 
+             switch(peek().type) {
+                 case CLASS:
+                 case FUN:
+                 case VAR:
+                 case FOR:
+                 case IF:
+                 case WHILE:
+                 case PRINT:
+                 case RETURN:
+                     return ;
+             }
+
+             advance();
+         }
+    }
 }
