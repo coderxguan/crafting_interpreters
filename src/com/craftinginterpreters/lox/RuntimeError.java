d0ae604583f858f96171af2f11ea4bda7206d100
coderxguan <3288733739@qq.com>
2024-09-19 16:05:06 +0800

    complete chapter 7

---
+++ src/com/craftinginterpreters/lox/RuntimeError.java
@@ -0,0 +1,15 @@
+package com.craftinginterpreters.lox;
+
+/**
+ * 功能:
+ * 作者:
+ * 日期:2024/09/19 15:43
+ */
+public class RuntimeError extends RuntimeException {
+    final Token token;
+
+    RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
